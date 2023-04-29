fa03977e4d2e732e7689f650e0d9eed855a2927a
PEDR0Lcs <pedrosoutoalbuquerque1@gmail.com>
2023-04-29 00:09:50 -0300

    dia 3

--- atvdpy.py
+++ atvdpy.py
@@ -5,21 +5,75 @@
 # nota1 = float(input('digite sua nota:' ))
 # nota2 = float(input('digite sua nota:' ))
 # nota3 = float(input('digite sua nota:' ))
-# print((nota1+nota2+nota3)/3)
+# impressão((nota1+nota2+nota3)/3)
 
-# import random
+# importar aleatoriamente
 # lista = [1, 2, 3, 4]
 # sorteio = random.choice(lista)
 # print('o numero sorteado foi:', sorteio)
 
 # numero= int(input('digite um número: '))
-# if numero % 2==1 :
+# se numero % 2==1 :
 #     print("seu numero é impar")
-# else :
+# mais :
 #     print("seu numero é par")
 
 # km = float(input('digite a distância em Km:'))
-# if km >= 200:
+# se km >= 200:
 #     print('o valor da passagem será: R$', km * 1.50)
-# else:
+# mais:
 #     print('o valor da passagem será: R$', km * 1.25)
+
+# HOME OFFICE
+
+# n1 = float(input('Digite qualquer número: '))
+# n2 = float(input('Digite qualquer número: '))
+# n3 = float(input('Digite qualquer número: '))
+
+# se n1 > n2 e n1 > n3 :
+#     print('Os três números são diferentes!')
+# elif n1 > n2 e n1 < n3 :
+#    print('Os três números são diferentes!')
+# elif n1 < n2 e n2 > n3 :
+#     print('Os três números são diferentes!')
+# elif n1 == n2 e n1 == n3 :
+#     print('Os três números são iguais!')
+# elif n1 == n2 e n1 != n3 :
+#     print('Dois números iguais!')
+# elif n1 != n2 e n1 == n3 :
+#     print('Dois números iguais!')
+# elif n1 != n2 e n2 == n3 :
+#     print('Dois números iguais!')
+# mais:
+#    print('ERRO')
+
+# num1= float(input('Digite um número: '))
+# num2= float(input('Digite um número: '))
+# num3= float(input('Digite um número: '))
+# num4= float(input('Digite um número: '))
+# num5= float(input('Digite um número: '))
+# maior_numero = num1 
+# se num2 > maior_numero: 
+#  maior_numero = num2 
+# elif num3 > maior_numero:      
+#   maior_numero = num3 
+# elif num4 > maior_numero:     
+#    maior_numero = num4  
+# elif num5 > maior_numero:     
+#    maior_numero = num5  
+# mais:
+#    print('ERRO')
+#    print("O maior número é: ", maior_numero)
+
+# n1 = float(input('Digite um número: '))
+# if n1 < 0 :
+#     print('O número{} é NEGATIVO!' .format(n1))
+# else :
+#     print('O número {} é POSITIVO!' .format(n1))
+
+# n1 = int(input('Digite um número: '))
+# if n1 %2 == 0 :
+#     print('O número {} é PAR' .format(n1))
+# else :
+#     print('O número {} é ÍMPAR' .format(n1))
+
